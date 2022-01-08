d51381ee76e4cbc803335665f0c1771747d07926
codesirohi <siddhantshubham23@gmail.com>
2022-01-08 18:18:24 +0530

    added (edit and remove)

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -1,3 +1,4 @@
+//store data to local storage
 function submitData() {
   let id_ls, user_ls, age_ls;
 
@@ -11,31 +12,48 @@ function submitData() {
     : [];
 
   userRecords.push({
-      ID: id_ls,
-      Name: user_ls,
-      Age: age_ls,
-    });
-  
+    ID: id_ls,
+    Name: user_ls,
+    Age: age_ls,
+  });
 
   localStorage.setItem("users", JSON.stringify(userRecords));
   document.getElementById("myForm").reset();
 }
- 
 
+//build table function
 
-
-function buildTable(data){
-var table = document.getElementById('myTable')
-table.innerHTML=''
-for (var i = 0; i < data.length; i++){
-var row = `<tr>
+function buildTable(data) {
+  var table = document.getElementById("myTable");
+  table.innerHTML = "";
+  for (var i = 0; i < data.length; i++) {
+    var row = `<tr>
         <td>${data[i].ID}</td>
         <td>${data[i].Name}</td>
         <td>${data[i].Age}</td>
-        </tr>`
+        <td>
+        <span>
+          <button class="btn1 list">Edit</button>
+          <button class="btn1 list">Remove</button>
+        </span>
+      </td>
 
-    table.innerHTML += row
+        </tr>`;
 
-
+    table.innerHTML += row;
+  }
 }
+///search function
+function searchTable(value, data) {
+  var filteredData = [];
+
+  for (var i = 0; i < data.length; i++) {
+    value = value.toLowerCase();
+    var name = data[i].ID.toLowerCase();
+    if (name.includes(value)) {
+      filteredData.push(data[i]);
+    }
+  }
+
+  return filteredData;
 }
